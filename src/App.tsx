beb700e6193d5a6acae279b195153acf2c5e8a47
unknown <tamzid620@gmail.com>
2026-05-15 00:31:32 +0600

    incremental hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { useDispatch, useSelector } from "react-redux";
+
 import "./App.css";
-import { decrement, increment } from "./redux/features/couterSlice";
+import { decrement, increment, incrementByFive } from "./redux/features/couterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
 
-  const count = useSelector((state) => state.counter.count);
-const dispatch = useDispatch();
+  const count = useAppSelector((state) => state.counter.count);
+const dispatch = useAppDispatch();
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="border border-gray-300 rounded-sm flex items-center gap-5 px-5 py-6">
+        <button 
+          onClick={() => dispatch(incrementByFive(5))}
+        className="px-5 py-2 bg-green-500 hover:bg-green-800 hover:text-white font-semibold rounded-sm uppercase">
+          increment by 5
+        </button>
         <button 
           onClick={() => dispatch(increment())}
         className="px-5 py-2 bg-green-500 hover:bg-green-800 hover:text-white font-semibold rounded-sm uppercase">
--- src/redux/features/couterSlice.tsx
+++ src/redux/features/couterSlice.tsx
@@ -1,11 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-
+type CounterState = {
+    count: number ;
+}
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState: { count: 0 },
+    initialState: { count: 0 } as CounterState ,
     reducers: {
+        incrementByFive: (state, action :PayloadAction<number> ) => {
+          state.count=  state.count + action.payload ;
+        } ,
         increment: (state, action ) => {
           state.count=  state.count += 1 ;
         } ,
@@ -15,7 +20,7 @@ const counterSlice = createSlice({
     }
 }) ;
 
-export const { increment, decrement } = counterSlice.actions ;
+export const { increment, decrement , incrementByFive } = counterSlice.actions ;
 export default counterSlice.reducer ;
 
 
