150d60c43a9df0060fd9b8c948df66c096566ade
unknown <tamzid620@gmail.com>
2026-05-14 19:45:27 +0600

    basic increment decrement counterslice added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/couterSlice";
 
 function App() {
+
+  const count = useSelector((state) => state.counter.count);
+const dispatch = useDispatch();
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="border border-gray-300 rounded-sm flex items-center gap-5 px-5 py-6">
-        <button className="px-5 py-2 bg-green-500 hover:bg-green-800 hover:text-white font-semibold rounded-sm uppercase">
+        <button 
+          onClick={() => dispatch(increment())}
+        className="px-5 py-2 bg-green-500 hover:bg-green-800 hover:text-white font-semibold rounded-sm uppercase">
           increment
         </button>
-        <h1 className="text-lg font-semibold">0</h1>
-        <button className="px-5 py-2 bg-red-500 hover:bg-red-800 hover:text-white font-semibold rounded-sm uppercase">
+        <h1 className="text-lg font-semibold">{count}</h1>
+        <button 
+          onClick={() => dispatch(decrement())}
+          className="px-5 py-2 bg-red-500 hover:bg-red-800 hover:text-white font-semibold rounded-sm uppercase">
           decrement
         </button>
       </div>
---
+++ src/redux/features/couterSlice.tsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: { count: 0 },
+    reducers: {
+        increment: (state, action ) => {
+          state.count=  state.count += 1 ;
+        } ,
+        decrement: (state, action ) => {
+          state.count=  state.count -= 1 ;
+        }
+    }
+}) ;
+
+export const { increment, decrement } = counterSlice.actions ;
+export default counterSlice.reducer ;
+
+
